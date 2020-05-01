3fd923ce9039d6d744caa6174947a87d9ebd8d45
jiehao <jiehao.liu@mail.utoronto.ca>
2020-04-30 21:03:18 -0400

    Setup startRemoveExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import {startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses} from '../../actions/expenses';
+import {startAddExpense, addExpense, editExpense, removeExpense, startRemoveExpense, setExpenses, startSetExpenses} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase.js';
 
@@ -108,4 +108,21 @@ test('Should fetch the expenses from firebase', (done) => {
         });
         done();
     });
+});
+
+test('Should remove expenses from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id;
+    store.dispatch(startRemoveExpense({id})).then(()=>{
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        });
+        
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot)=>{
+            expect(snapshot.val()).toBeFalsy(); //toEqual(null);
+            done();
+    }); 
 });
